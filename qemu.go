fd54ed67c916b7e5c6837c4a77fe255075ccf66c
James O. D. Hunt <james.o.hunt@intel.com>
2017-03-10 14:43:51 +0000

    qemu: Add support for 'q35' machine type.
    Retain the default 'pc-lite' machine type, but now also support 'q35' as
    an option. No other machine types are currently supported.
    
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

--- qemu.go
+++ qemu.go
@@ -54,6 +54,19 @@ type qemu struct {
 
 const defaultQemuPath = "/usr/bin/qemu-system-x86_64"
 
+const defaultQemuMachineType = "pc-lite"
+
+var supportedQemuMachines = []ciaoQemu.Machine{
+	{
+		Type:         defaultQemuMachineType,
+		Acceleration: "kvm,kernel_irqchip,nvdimm",
+	},
+	{
+		Type:         "q35",
+		Acceleration: "kvm,kernel_irqchip,nvdimm",
+	},
+}
+
 const (
 	defaultSockets uint32 = 1
 	defaultThreads uint32 = 1
@@ -319,6 +332,16 @@ func (q *qemu) forceUUIDFormat(str string) string {
 	return uuidSlice.String()
 }
 
+func (q *qemu) getMachine(name string) (ciaoQemu.Machine, error) {
+	for _, m := range supportedQemuMachines {
+		if m.Type == name {
+			return m, nil
+		}
+	}
+
+	return ciaoQemu.Machine{}, fmt.Errorf("unrecognised machine type: %v", name)
+}
+
 // init intializes the Qemu structure.
 func (q *qemu) init(config HypervisorConfig) error {
 	valid, err := config.valid()
@@ -410,9 +433,14 @@ func (q *qemu) setMemoryResources(podConfig PodConfig) ciaoQemu.Memory {
 func (q *qemu) createPod(podConfig PodConfig) error {
 	var devices []ciaoQemu.Device
 
-	machine := ciaoQemu.Machine{
-		Type:         "pc-lite",
-		Acceleration: "kvm,kernel_irqchip,nvdimm",
+	machineType := q.config.HypervisorMachineType
+	if machineType == "" {
+		machineType = defaultQemuMachineType
+	}
+
+	machine, err := q.getMachine(machineType)
+	if err != nil {
+		return err
 	}
 
 	smp := q.setCPUResources(podConfig)
@@ -463,7 +491,7 @@ func (q *qemu) createPod(podConfig PodConfig) error {
 
 	devices = q.appendFSDevices(devices, podConfig)
 	devices = q.appendConsoles(devices, podConfig)
-	devices, err := q.appendImage(devices, podConfig)
+	devices, err = q.appendImage(devices, podConfig)
 	if err != nil {
 		return err
 	}
